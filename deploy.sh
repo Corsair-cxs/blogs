3db483223df17a27fa6f5231ddba78b57b7f6b89
Corsair-cxs <1494089474@qq.com>
2023-01-11 17:07:54 +0800

    rebuilding site 2023年01月11日 17:07:54

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,16 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # 构建静态内容
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# 添加更改到 git
+git add .
+
+# 提交更改
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+msg="$*"
+fi
+git commit -m "$msg"
+
 # 切换到 Public 文件夹
 cd public
 
@@ -22,4 +32,10 @@ fi
 git commit -m "$msg"
 
 # 推送到远程仓库
-git push origin main
+git push origin 
+
+# 切换到 Public 文件夹
+cd ..
+
+# 推送到远程仓库
+git push origin 
